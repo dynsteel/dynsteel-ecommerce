0416fb627ac48af394297dd0e9148c96c41a01ff
dynsteel <steeldyn@gmail.com>
2025-11-25 23:28:47 +0300

    Fix import paths for MongoDB connection - Correct relative paths

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,4 +1,4 @@
-import clientPromise from '../../../../lib/mongodb'
+import clientPromise from '../../../lib/mongodb'
 import bcrypt from 'bcryptjs'
 
 export default async function handler(req, res) {
